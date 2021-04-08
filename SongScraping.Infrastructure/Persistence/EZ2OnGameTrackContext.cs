caaa98f209b37064484f028ed91a45ca1a7645a2
Ez2raven <ez2raven@gmail.com>
2021-04-08 22:07:19 +0800

    Added infra layer placeholder for persisting songs.

---
+++ SongScraping.Infrastructure/Persistence/EZ2OnGameTrackContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MusicGames.SongScraping.Persistence
+{
+    public class Ez2OnGameTrackContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=C:\streamer-site.db");
+        }
+    }
+}
