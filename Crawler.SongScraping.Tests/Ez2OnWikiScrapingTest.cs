c253153eb3de65ec080bb9094b65903563965491
Ez2Raven <ez2raven@gmail.com>
2022-10-03 16:31:44 +0800

    Updated unit tests for clarity and built in assumptions.

--- Crawler.SongScraping.Tests/Ez2OnWikiScrapingTest.cs
+++ Crawler.SongScraping.Tests/Ez2OnWikiScrapingTest.cs
@@ -5,6 +5,7 @@ using Crawler.SongScraping.Aggregators.Ez2OnWiki.Scrapers;
 using Crawler.SongScraping.Interpreters.Ez2On;
 using Crawler.SongScraping.Interpreters.Generic;
 using FluentAssertions;
+using Gaming.Domain.AggregateModels.SongChartAggregate;
 using Gaming.Domain.AggregateModels.SongChartAggregate.EqualityComparer;
 using HtmlAgilityPack;
 using Microsoft.Extensions.Logging;
@@ -47,17 +48,21 @@ public class Ez2OnWikiScrapingTest
         var songAlbumInterpreter = new SongAlbumInterpreter();
         var gameTitleInterpreter = new Ez2OnReleaseInterpreter();
         var chartLevelInterpreter = new ChartLvInterpreter(new Mock<ILogger<ChartLvInterpreter>>().Object);
-        var parser =
-            new SongChartCollectionParser(songTitleInterpreter, songAlbumInterpreter, gameTitleInterpreter, chartLevelInterpreter);
-        var songChartCollectionScraper = new SongChartCollectionScraper(loggerStub.Object, parser);
+        var songChartParser =
+            new SongChartCollectionParser(songTitleInterpreter, songAlbumInterpreter, gameTitleInterpreter,
+                chartLevelInterpreter);
+        var songChartCollectionScraper = new SongChartCollectionScraper(loggerStub.Object, songChartParser);
 
         var songCharts = songChartCollectionScraper.Run(ez2onLevelListUrl);
 
         songCharts.Count.Should().BeGreaterThan(0);
-        foreach (var chart in songCharts)
-        {
-            _output.WriteLine(chart.ToString());
-        }
+        songCharts.Should().NotContain(x => string.IsNullOrWhiteSpace(x.Game.Name));
+        songCharts.Should().Contain(x => Equals(x.Game.IsDlc, true));
+        songCharts.Should().Contain(x => Equals(x.Game.IsDlc, false));
+        songCharts.Should().NotContain(x => Equals(x.DifficultyMode.Category, DifficultyCategory.None));
+        songCharts.Should().Contain(x => x.DifficultyMode.Level > 0);
+        songCharts.Should().NotContain(x => Equals(x.DifficultyMode.KeyMode, KeyModes.None));
+        songCharts.Should().NotContain(x => string.IsNullOrWhiteSpace(x.Song.Title));
     }
 
     [Fact]
@@ -71,20 +76,66 @@ public class Ez2OnWikiScrapingTest
         var gameTitleInterpreter = new Ez2OnReleaseInterpreter();
         var chartLevelInterpreter = new ChartLvInterpreter(new Mock<ILogger<ChartLvInterpreter>>().Object);
         var songChartCollectionParser =
-            new SongChartCollectionParser(songTitleInterpreter, songAlbumInterpreter, gameTitleInterpreter, chartLevelInterpreter);
+            new SongChartCollectionParser(songTitleInterpreter, songAlbumInterpreter, gameTitleInterpreter,
+                chartLevelInterpreter);
         var songChartScraper = new SongChartCollectionScraper(loggerStub.Object, songChartCollectionParser);
         var songListParser = new SongListCollectionParser();
         var songListLinkedScraper = new SongListLinkedScraper(songChartScraper, songListParser);
 
         var songCharts = songListLinkedScraper.AddSongMetaData(songListUrl, levelListUrl);
         songCharts.Count.Should().BeGreaterThan(0);
+        songCharts.Should().NotContain(x => string.IsNullOrWhiteSpace(x.Game.Name));
+        songCharts.Should().Contain(x => Equals(x.Game.IsDlc, true));
+        songCharts.Should().Contain(x => Equals(x.Game.IsDlc, false));
+        songCharts.Should().NotContain(x => Equals(x.DifficultyMode.Category, DifficultyCategory.None));
+        songCharts.Should().Contain(x => x.DifficultyMode.Level > 0);
+        songCharts.Should().NotContain(x => Equals(x.DifficultyMode.KeyMode, KeyModes.None));
+        songCharts.Should().NotContain(x => string.IsNullOrWhiteSpace(x.Song.Title));
+        songCharts.Should().NotContain(x => string.IsNullOrWhiteSpace(x.Song.Album));
+        // contains at least 1 non-null record for BPM, Genre and composer
+        songCharts.Should().Contain(x => !string.IsNullOrWhiteSpace(x.Song.Bpm));
+        songCharts.Should().Contain(x => !string.IsNullOrWhiteSpace(x.Song.Genre));
+        songCharts.Should().Contain(x => !string.IsNullOrWhiteSpace(x.Song.Composer));
+    }
 
-        var filteredSongCharts = songCharts.Where(songChart => songChart.Song.Composer.ToUpper() == "M2U").ToList();
+    [Theory]
+    [InlineData("M2U")]
+    public void Run_SongListDecorator_ReturnsListOfSongChartsWithSongDetailsOfSpecificComposer(string composer)
+    {
+        var songListUrl = "https://wikiwiki.jp/ez2on/SongList";
+        var levelListUrl = "https://wikiwiki.jp/ez2on/LevelList/List";
+        var loggerStub = new Mock<ILogger<SongChartCollectionScraper>>();
+        var songTitleInterpreter = new SongTitleInterpreter();
+        var songAlbumInterpreter = new SongAlbumInterpreter();
+        var gameTitleInterpreter = new Ez2OnReleaseInterpreter();
+        var chartLevelInterpreter = new ChartLvInterpreter(new Mock<ILogger<ChartLvInterpreter>>().Object);
+        var songChartCollectionParser =
+            new SongChartCollectionParser(songTitleInterpreter, songAlbumInterpreter, gameTitleInterpreter,
+                chartLevelInterpreter);
+        var songChartScraper = new SongChartCollectionScraper(loggerStub.Object, songChartCollectionParser);
+        var songListParser = new SongListCollectionParser();
+        var songListLinkedScraper = new SongListLinkedScraper(songChartScraper, songListParser);
+
+        var songCharts = songListLinkedScraper.AddSongMetaData(songListUrl, levelListUrl);
+        var filteredSongCharts = songCharts.Where(songChart => songChart.Song.Composer.ToUpper() == composer).ToList();
+
         filteredSongCharts.Count.Should().BeGreaterThan(0);
+        filteredSongCharts.Should().NotContain(x => string.IsNullOrWhiteSpace(x.Game.Name));
+        filteredSongCharts.Should().Contain(x => Equals(x.Game.IsDlc, true));
+        filteredSongCharts.Should().Contain(x => Equals(x.Game.IsDlc, false));
+        filteredSongCharts.Should().NotContain(x => Equals(x.DifficultyMode.Category, DifficultyCategory.None));
+        filteredSongCharts.Should().Contain(x => x.DifficultyMode.Level > 0);
+        filteredSongCharts.Should().NotContain(x => Equals(x.DifficultyMode.KeyMode, KeyModes.None));
+        filteredSongCharts.Should().NotContain(x => string.IsNullOrWhiteSpace(x.Song.Title));
+        filteredSongCharts.Should().NotContain(x => string.IsNullOrWhiteSpace(x.Song.Album));
+        // contains at least 1 non-null record for BPM, Genre and composer
+        filteredSongCharts.Should().Contain(x => !string.IsNullOrWhiteSpace(x.Song.Bpm));
+        filteredSongCharts.Should().Contain(x => !string.IsNullOrWhiteSpace(x.Song.Genre));
+        filteredSongCharts.Should().OnlyContain(x => Equals(x.Song.Composer, composer));
         foreach (var songChart in filteredSongCharts.OrderBy(songChart => songChart.Song.Title)
                      .Distinct(new SongTitleComparer()))
         {
-            _output.WriteLine($"{songChart.Song.Title} - {songChart.Song.Genre}");
+            _output.WriteLine($"{songChart.Song}");
         }
     }
 }
